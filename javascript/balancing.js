e146f99599b6a2bfb5d0284a0807b34861d82d10
RomanLatipov <romanlatipov@gmail.com>
2024-07-10 23:39:27 -0400

    complete balanced-binary-tree lab

--- javascript/balancing.js
+++ javascript/balancing.js
@@ -7,13 +7,27 @@ class Node {
 }
 
 // list = [3, 5, 6, 9, 10, 20]
+          //  9
+        //  5   10   
+      //  3  6    20
 function threeToTwenty() {
+  const node1 = new Node(3);
+  const node2 = new Node(6);
+  const node3 = new Node(5, node1, node2);
+  const node4 = new Node(20);
+  const node5 = new Node(10, node4);
 
+  return new Node(9, node3, node5);
 }
 
 // list = [10, 11, 30, 100, 200]
 function tenToTwoHundred() {
+  const node1 = new Node(10);
+  const node2 = new Node(11, node1);
+  const node3 = new Node(200);
+  const node4 = new Node(100, null, node3);
 
+  return new Node(30, node2, node4);
 }
 
 if (require.main === module) {
